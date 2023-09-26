9b57bf7a68402eb13356c1eac9b5a141eacf2f39
DeadZoneReforger <136852459+DeadZoneReforger@users.noreply.github.com>
2023-09-26 15:21:24 -0600

    shop fix for latest update

---
+++ Scripts/Game/DZ_ADM_PhysicalShopComponent.c
@@ -0,0 +1,8 @@
+modded class ADM_PhysicalShopComponentClass: ADM_PhysicalShopComponentClass {}
+
+//TODO: look into SCR_PreviewEntityComponent
+//TODO: fix respawn time = 0 bug (vehicle check doesnt work)
+modded class ADM_PhysicalShopComponent: ADM_ShopBaseComponent
+{	
+
+}
