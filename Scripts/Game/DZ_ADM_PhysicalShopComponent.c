69f8a69b69e7acc46ec690fc4420009ec6e17c75
DeadZoneReforger <136852459+DeadZoneReforger@users.noreply.github.com>
2023-11-08 12:02:52 -0600

    Delete Scripts/Game/DZ_ADM_PhysicalShopComponent.c no longer needed

--- Scripts/Game/DZ_ADM_PhysicalShopComponent.c
+++
@@ -1,8 +0,0 @@
-modded class ADM_PhysicalShopComponentClass: ADM_PhysicalShopComponentClass {}
-
-//TODO: look into SCR_PreviewEntityComponent
-//TODO: fix respawn time = 0 bug (vehicle check doesnt work)
-modded class ADM_PhysicalShopComponent: ADM_ShopBaseComponent
-{	
-
-}
